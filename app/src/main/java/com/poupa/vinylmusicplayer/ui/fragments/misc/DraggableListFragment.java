c85e141fe66dceac9b1bac5a6d74ab7e08db42ef
Octoton <mdums@hotmail.fr>
2021-08-22 18:19:27 +0200

    Bottom sheet visual mockup done:  - Could add a title to history size  - CategoryInfoAdapter should extend of DraggableListAdapter if possible  - Add visual logic (criteria gray out if random first, not selected, ...)  - Add saving to new album shuffling preference  - Split criteria visual and logic to new interface  - Forward result to dynamicElement  - Clean visual

--- app/src/main/java/com/poupa/vinylmusicplayer/ui/fragments/misc/DraggableListFragment.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/ui/fragments/misc/DraggableListFragment.java
@@ -1,20 +1,28 @@
 package com.poupa.vinylmusicplayer.ui.fragments.misc;
 
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.PopupMenu;
 
 import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.h6ah4i.android.widget.advrecyclerview.animator.DraggableItemAnimator;
@@ -23,6 +31,7 @@ import com.h6ah4i.android.widget.advrecyclerview.draggable.RecyclerViewDragDropM
 import com.h6ah4i.android.widget.advrecyclerview.utils.WrapperAdapterUtils;
 import com.poupa.vinylmusicplayer.R;
 import com.poupa.vinylmusicplayer.adapter.misc.DraggableListAdapter;
+import com.poupa.vinylmusicplayer.util.PreferenceUtil;
 
 
 public class DraggableListFragment extends BottomSheetDialogFragment {
@@ -50,6 +59,39 @@ public class DraggableListFragment extends BottomSheetDialogFragment {
         super.onDestroyView();
     }
 
+    @NonNull @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        BottomSheetDialog dialog = new BottomSheetDialog(getActivity());
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                BottomSheetDialog d = (BottomSheetDialog) dialog;
+                FrameLayout bottomSheet = d.findViewById(com.google.android.material.R.id.design_bottom_sheet);
+
+                BottomSheetBehavior behaviour = BottomSheetBehavior.from(bottomSheet);
+                behaviour.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                behaviour.setDraggable(false);
+                behaviour.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+                    @Override public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                        if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                            behaviour.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                        }
+                    }
+
+                    @Override
+                    public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+                    }
+                });
+            }
+        });
+
+        return dialog;
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -73,15 +115,25 @@ public class DraggableListFragment extends BottomSheetDialogFragment {
         recyclerViewDragDropManager.attachRecyclerView(recyclerView);
 
         Button button = view.findViewById(R.id.btnShow);
+        button.setText("Album");
         button.setOnClickListener(v -> {
             showMenu(this.getContext(), v, R.menu.menu_dynamic_element_type);
         });
 
-        View bottomSheet = view.findViewById(R.id.toto);
-        final TypedValue typedColorBackground = new TypedValue();
-        //this.getTheme().resolveAttribute(R.attr.cardBackgroundColor, typedColorBackground, true);
-        @ColorInt int color = typedColorBackground.data;
-        bottomSheet.setBackgroundColor(color);
+
+        view.findViewById(R.id.cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        view.findViewById(R.id.ok).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
 
         return view;
     }
@@ -90,7 +142,14 @@ public class DraggableListFragment extends BottomSheetDialogFragment {
         PopupMenu popupMenu = new PopupMenu(context, view);
 
         popupMenu.inflate(menuRes);
-        //popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Button button = view.findViewById(R.id.btnShow);
+                button.setText(menuItem.getTitle());
+                return true;
+            }
+        });
 
         popupMenu.show();
     }
