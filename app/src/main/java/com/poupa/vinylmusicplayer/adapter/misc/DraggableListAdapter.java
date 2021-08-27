ea5cf4c15087e33f8418f6002eb3c11c4e85ac96
Octoton <mdums@hotmail.fr>
2021-08-28 00:19:14 +0200

    Fusion between CategoryInfo & SelectableItem

--- app/src/main/java/com/poupa/vinylmusicplayer/adapter/misc/DraggableListAdapter.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/adapter/misc/DraggableListAdapter.java
@@ -1,16 +1,16 @@
 package com.poupa.vinylmusicplayer.adapter.misc;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,23 +18,17 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 import com.poupa.vinylmusicplayer.R;
 import com.poupa.vinylmusicplayer.databinding.PreferenceDialogLibraryCategoriesListitemBinding;
+import com.poupa.vinylmusicplayer.model.CategoryInfo;
 import com.poupa.vinylmusicplayer.util.SwipeAndDragHelper;
 
 
-public class DraggableListAdapter extends RecyclerView.Adapter<DraggableListAdapter.ViewHolder> implements SwipeAndDragHelper.ActionCompletionContract {
+public class DraggableListAdapter<T extends SelectableItem<U>, U extends Serializable> extends RecyclerView.Adapter<DraggableListAdapter.ViewHolder> implements SwipeAndDragHelper.ActionCompletionContract {
 
-    private ArrayList<Item> listItem;
-    private final ItemTouchHelper touchHelper;
+    protected ArrayList<T> listItem;
+    protected final ItemTouchHelper touchHelper;
 
-    // next step:
-    // public DraggableListAdapter(@NonNull ArrayList<Item> objects) {
-    // as fragment will remember what you have selected (with parcelable)
-    public DraggableListAdapter(@NonNull String[] objects) {
-        listItem = new ArrayList<>();
-
-        for (String object : objects) {
-            listItem.add(new Item(object, true));
-        }
+    public DraggableListAdapter(@NonNull ArrayList<T> objects) {
+        listItem = objects;
 
         SwipeAndDragHelper swipeAndDragHelper = new SwipeAndDragHelper(this);
         touchHelper = new ItemTouchHelper(swipeAndDragHelper);
@@ -42,7 +36,7 @@ public class DraggableListAdapter extends RecyclerView.Adapter<DraggableListAdap
 
     @Override
     public void onViewMoved(int oldPosition, int newPosition) {
-        Item item = listItem.remove(oldPosition);
+        T item = listItem.remove(oldPosition);
         listItem.add(newPosition, item);
 
         notifyItemMoved(oldPosition, newPosition);
@@ -64,9 +58,17 @@ public class DraggableListAdapter extends RecyclerView.Adapter<DraggableListAdap
         return new ViewHolder(binding);
     }
 
-    private boolean isLastCheckedItem(Item item) {
+    public ArrayList<T> getSelectableItems() { return listItem; }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void setSelectableItems(ArrayList<T> listItem) {
+        this.listItem = listItem;
+        notifyDataSetChanged();
+    }
+
+    protected boolean isLastCheckedItem(T item) {
         if (item.visible) {
-            for (Item i : listItem) {
+            for (T i : listItem) {
                 if (i != item && i.visible) return false;
             }
         }
@@ -76,37 +78,36 @@ public class DraggableListAdapter extends RecyclerView.Adapter<DraggableListAdap
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public void onBindViewHolder(@NonNull DraggableListAdapter.ViewHolder holder, int position) {
-        Item o = listItem.get(position);
+        T o = listItem.get(position);
 
-        holder.title.setText(o.item);
+        holder.title.setText(o.toString());
 
         holder.checkBox.setChecked(o.visible);
 
-        holder.itemView.setOnClickListener(v -> {
-            if (!(o.visible && isLastCheckedItem(o))) {
-                o.visible = !o.visible;
-                holder.checkBox.setChecked(o.visible);
-            } else {
-                Toast.makeText(holder.itemView.getContext(), R.string.you_have_to_select_at_least_one_category, Toast.LENGTH_SHORT).show();
+        holder.dragView.setOnTouchListener((view, event) -> {
+            if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+                touchHelper.startDrag(holder);
             }
+            return false;
         });
 
-        holder.dragView.setOnTouchListener((view, event) -> {
-                    if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
-                        touchHelper.startDrag(holder);
-                    }
-                    return false;
-                }
-        );
+        holder.itemView.setOnClickListener(v -> {
+            checkBoxOnClick(holder, o);
+        });
+    }
+
+    protected void checkBoxOnClick(@NonNull DraggableListAdapter.ViewHolder holder, T object) {
+        object.visible = !object.visible;
+        holder.checkBox.setChecked(object.visible);
     }
 
     public void attachToRecyclerView(RecyclerView recyclerView) {
         touchHelper.attachToRecyclerView(recyclerView);
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         public final View dragView;
-        public final com.kabouzeid.appthemehelper.common.views.ATECheckBox checkBox;
+        public final CheckBox checkBox;
         public final TextView title;
 
         public ViewHolder(@NonNull PreferenceDialogLibraryCategoriesListitemBinding binding) {
@@ -117,42 +118,5 @@ public class DraggableListAdapter extends RecyclerView.Adapter<DraggableListAdap
             checkBox = binding.checkbox;
 
         }
-    }
-
-    public class Item { //implements Parcelable {
-        public String item;
-        public boolean visible;
-
-        public Item(String item, boolean visible) {
-            this.item = item;
-            this.visible = visible;
-        }
-
-        /*private item(Parcel source) {
-            item = (String) source.readSerializable();
-            visible = source.readInt() == 1;
-        }
-
-
-        @Override
-        public int describeContents() {
-            return 0;
-        }
-
-        public void writeToParcel(Parcel dest, int flags) {
-            dest.writeSerializable(category);
-            dest.writeInt(visible ? 1 : 0);
-        }
-
-        public static final Parcelable.Creator<com.poupa.vinylmusicplayer.model.CategoryInfo> CREATOR = new Parcelable.Creator<com.poupa.vinylmusicplayer.model.CategoryInfo>() {
-            public com.poupa.vinylmusicplayer.model.CategoryInfo createFromParcel(Parcel source) {
-                return new com.poupa.vinylmusicplayer.model.CategoryInfo(source);
-            }
-
-            public com.poupa.vinylmusicplayer.model.CategoryInfo[] newArray(int size) {
-                return new com.poupa.vinylmusicplayer.model.CategoryInfo[size];
-            }
-        }; */
-
     }
 }
--- app/src/main/java/com/poupa/vinylmusicplayer/model/CategoryInfo.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/model/CategoryInfo.java
@@ -1,44 +1,21 @@
 package com.poupa.vinylmusicplayer.model;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
+import com.poupa.vinylmusicplayer.App;
 import com.poupa.vinylmusicplayer.R;
+import com.poupa.vinylmusicplayer.adapter.misc.SelectableItem;
+import com.poupa.vinylmusicplayer.model.CategoryInfo.Category;
 
-public class CategoryInfo implements Parcelable {
-    public final Category category;
-    public boolean visible;
+
+public class CategoryInfo extends SelectableItem<Category> {
 
     public CategoryInfo(Category category, boolean visible) {
-        this.category = category;
-        this.visible = visible;
+        super(category, visible);
     }
-
-    private CategoryInfo(Parcel source) {
-        category = (Category) source.readSerializable();
-        visible = source.readInt() == 1;
-    }
-
 
     @Override
-    public int describeContents() {
-        return 0;
+    public String toString() {
+        return App.getStaticContext().getResources().getString(item.stringRes);
     }
-
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeSerializable(category);
-        dest.writeInt(visible ? 1 : 0);
-    }
-
-    public static final Parcelable.Creator<CategoryInfo> CREATOR = new Parcelable.Creator<CategoryInfo>() {
-        public CategoryInfo createFromParcel(Parcel source) {
-            return new CategoryInfo(source);
-        }
-
-        public CategoryInfo[] newArray(int size) {
-            return new CategoryInfo[size];
-        }
-    };
 
     public enum Category {
         SONGS(R.string.songs),
