318c3a204f60f4262b0a9247fa005ae43fbd771e
Octoton <mdums@hotmail.fr>
2021-09-04 21:26:16 +0200

    Add first iteration of dynamic element style (only work for french)

--- app/src/main/java/com/poupa/vinylmusicplayer/ui/fragments/misc/TestFragment.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/ui/fragments/misc/TestFragment.java
@@ -5,18 +5,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 import com.poupa.vinylmusicplayer.R;
+import com.poupa.vinylmusicplayer.util.DynamicElement.AlbumShufflingUtil;
+
+import static java.lang.Integer.parseInt;
 
 
-public class TestFragment extends Fragment {
+public class TestFragment extends DynamicElementPreferenceFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_test,
                 container, false);
         return view;
+    }
+
+    public void reset() {
+
+    }
+
+    public void ok() {
+
     }
 
 }
